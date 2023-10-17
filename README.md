3145d8066583f58c15b5478f57cd7d9acb7bd1fc
Luiz Henrique <luiz.henrique.mm@hotmail.com>
2023-10-17 20:28:06 -0300

    changes

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Creating the image:
 ```bash
 docker build . -t 3.4.1
 ```
+
+# Changing the volumes path
+
+In all volumes, you must change the path for the repo clone path.
--- spark/conf/spark-env.sh
+++ spark/conf/spark-env.sh
@@ -5,3 +5,5 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
