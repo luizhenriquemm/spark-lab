80325bf3337a7a8387f5186c19cb1fe8c16c4b00
luizhenriquemm <luiz.henrique.mm@hotmail.com>
2023-10-17 19:04:53 -0300

    changes

---
+++ spark/conf/spark-env.sh
@@ -0,0 +1,7 @@
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
