c363058bdf4c8703704adb01f401b12593eb06de
Luiz Henrique <luiz.henrique.mm@hotmail.com>
2023-10-21 16:49:38 -0300

    Update spark-env.sh

--- spark/conf/spark-env.sh
+++ spark/conf/spark-env.sh
@@ -17,3 +17,5 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
