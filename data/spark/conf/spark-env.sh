82e0d5c26e08cce97b9973aa0de4131e7bc819a8
Luiz Henrique <luiz.henrique.mm@hotmail.com>
2026-05-24 23:25:33 -0300

    Begin ws-dados-temperlandia

--- data/spark/conf/spark-env.sh
+++ data/spark/conf/spark-env.sh
@@ -171,3 +171,7 @@ LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
 LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
+LD_PRELOAD=/opt/bitnami/common/lib/libnss_wrapper.so
---
+++ ws-dados-temper.md
@@ -0,0 +1,4 @@
+
+
+docker compose up -d jupyter spark-master spark-worker-0 postgres hive-metastore minio zookeeper
+
